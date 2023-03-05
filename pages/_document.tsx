494a4c52817091640a976f10c31b52962233dfbc
WahidHoque <wahidhoquee@gmail.com>
2023-03-05 22:22:18 +0600

    Added meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,23 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="subject"
+          content="United State Chronic Disease Interactive Data Story"
+        />
+        <meta
+          name="description"
+          content=" Explore US Chronic Disease prevalence and death rates. Interactively compare diseases along with race, sex and age."
+        />
+        <meta property="og:title" content="The Rock" />
+        <meta
+          property="og:url"
+          content="https://blending101.com/viz/chronic-morbidity/"
+        />
+        <meta
+          property="og:image"
+          content="https://images.pexels.com/photos/15521519/pexels-photo-15521519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
